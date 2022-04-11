b556f842edadfd52d73e42b6cc7e43623df7b80d
刘沛琪 <19302010062@fudan.edu.cn>
2022-04-11 20:33:59 +0800

    test

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+// import { RouteRecordRaw } from 'vue-router';
+import TabsPage from '../views/TabsPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/tabs/learn'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/learn'
+      },
+      // {
+      //   path: 'review',
+      //   component: () => import('@/views/ReviewPage.vue')
+      // },
+      {
+        path: 'learn',
+        component: () => import('@/views/LearnPage.vue')
+      },
+      {
+        path: 'settings',
+        component: () => import('@/views/SettingsPage.vue')
+      }
+    ]
+  },
+  {
+    path: '/tabs/learnWord',
+    component: () => import('@/views/LearnWord.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/ReviewPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Review</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Review</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="Review page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'ReviewPage',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
